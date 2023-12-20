1decb132bb441b6466330a278fe2298a2d9ba341
coronelee <79922854+coronelee@users.noreply.github.com>
2023-12-20 20:03:00 +0300

    dev

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -2,7 +2,7 @@
 import styles from '../styles/MainWindow.module.scss'
 export default function MainWindow() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} id="main">
         <div className={styles.wrapper__container}>
             <div className={styles.container__Leftblock}>
                 <h5>Привет! Меня зовут</h5>
