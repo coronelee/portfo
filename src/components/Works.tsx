fb71cf86b8f4ce35399ece13ea519f3b744e3b74
coronelee <79922854+coronelee@users.noreply.github.com>
2024-01-26 11:23:59 +0300

    add

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -13,7 +13,10 @@ export default function Works() {
 
         <div className={styles.items}>
             <div className={styles.items__project}>
-                <a href="https://coronelee.github.io/transportcompany/"><div className={styles.project__preview}></div></a>
+                <a href="https://coronelee.github.io/transportcompany/"><div className={styles.project__preview} style={{backgroundImage:"../assets/work1.jpg"}}></div></a>
+            </div>
+            <div className={styles.items__project}>
+                <a href="https://github.com/coronelee/avion"><div className={styles.project__preview} style={{backgroundImage:"../assets/work2.jpeg"}}></div></a>
             </div>
         </div>
       </div>
